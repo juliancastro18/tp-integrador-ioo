ed51cb2ebb9cd010645d8bf411f40761e10f709a
kevin <kevincanepa2@gmail.com>
2020-09-11 16:35:33 -0300

    Se agrega clase Articulo,ItemCarrito, Carrito

---
+++ src/modelo/ItemCarrito.java
@@ -0,0 +1,36 @@
+package tpIntegrador;
+
+public class ItemCarrito{
+	private Articulo articulo;
+	private int cantidad;
+	public ItemCarrito(Articulo articulo, int cantidad) {
+		super();
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+	}
+	public ItemCarrito() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public boolean equals(ItemCarrito item) {
+		return this.getArticulo().getId() == item.getArticulo().getId();
+	}
+	
+	public String toString() {
+		return "Cantidad: " +cantidad+" "+articulo.toString();
+	}
+	
+}
